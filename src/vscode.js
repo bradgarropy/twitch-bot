db37fa5d3d1c83d037935ffd3c0bf22298a4a1b3
Brad Garropy <bradgarropy@gmail.com>
2021-01-04 13:05:21 -0600

    update license. choose random theme. change to windows path.

--- src/vscode.js
+++ src/vscode.js
@@ -1,10 +1,35 @@
 const fs = require("fs")
 const os = require("os")
+const {randomInteger} = require("./utils/random")
 
-const path = `${os.homedir()}/Library/Application Support/Code/User/settings.json`
+const path = `${os.homedir()}/AppData/Roaming/Code/User/settings.json`
 const settings = require(path)
 
+const themes = [
+    "Shades of Purple",
+    "Cobalt2",
+    "SynthWave '84",
+    "Night Owl",
+    "Dracula",
+    "In Bed by 7pm",
+    "morgan.codes",
+    "Level Up",
+    "Level Up Syntax",
+]
+
 const setTheme = theme => {
+    if (theme === "random") {
+        setRandomTheme()
+    } else {
+        settings["workbench.colorTheme"] = theme
+        fs.writeFileSync(path, JSON.stringify(settings, null, 4))
+    }
+}
+
+const setRandomTheme = () => {
+    const index = randomInteger(0, themes.length - 1)
+    const theme = themes[index]
+
     settings["workbench.colorTheme"] = theme
     fs.writeFileSync(path, JSON.stringify(settings, null, 4))
 }
