fd4f69c8c4b028db2b91dad79ed54791a2e94ada
Brad Garropy <bradgarropy@gmail.com>
2021-03-10 23:26:54 -0600

    remove playlist command.

--- src/commands/music.js
+++ src/commands/music.js
@@ -1,9 +1,7 @@
 const twitch = require("../twitch")
 
 twitch.on("message", (channel, tags, message) => {
-    const commands = ["!music", "!playlist"]
-
-    if (commands.includes(message)) {
+    if (message === "!music") {
         twitch.say(channel, "https://bradgarropy.com/codetunes")
     }
 })
