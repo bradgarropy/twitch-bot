c3a0c176006802a0bb3053511d41bab5418f09a6
Brad Garropy <bradgarropy@gmail.com>
2021-04-06 20:14:17 -0500

    add promo command.

---
+++ src/commands/promo.js
@@ -0,0 +1,15 @@
+const twitch = require("../twitch")
+
+twitch.on("message", (channel, tags, message) => {
+    if (message === "!promo") {
+        twitch.say(
+            channel,
+            `
+                https://bradgarropy.com/discord
+                https://bradgarropy.com/donate
+                https://bradgarropy.com/sponsor
+                https://bradgarropy.com/store
+            `,
+        )
+    }
+})
