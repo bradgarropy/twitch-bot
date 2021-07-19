fd084f3d5348a9de138ff6c2739ed2048e213c0b
Brad Garropy <bradgarropy@gmail.com>
2021-07-19 13:06:02 -0500

    update to use lamp lights.

--- src/redemptions/lights.js
+++ src/redemptions/lights.js
@@ -2,7 +2,7 @@ const hue = require("../hue")
 const twitch = require("../twitch")
 
 const id = "14e6be4e-b501-420f-a76c-e8fd2de2feba"
-const lights = [13, 14, 15, 16]
+const lights = [31, 32, 33]
 
 twitch.on("message", (channel, tags, message) => {
     if (tags["custom-reward-id"] === id) {
