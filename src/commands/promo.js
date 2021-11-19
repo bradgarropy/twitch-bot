e17db1ff6a42c60a582603491a98e95eb89f63c4
Brad Garropy <bradgarropy@gmail.com>
2021-11-18 20:42:33 -0600

    separate chat messages for each promo item.

--- src/commands/promo.js
+++ src/commands/promo.js
@@ -2,13 +2,8 @@ const twitch = require("../twitch")
 
 twitch.on("message", (channel, tags, message) => {
     if (message === "!promo") {
-        twitch.say(
-            channel,
-            `
-                https://bradgarropy.com/discord
-                https://bradgarropy.com/sponsor
-                https://bradgarropy.com/store
-            `,
-        )
+        twitch.say(channel, "https://bradgarropy.com/discord")
+        twitch.say(channel, "https://bradgarropy.com/sponsor")
+        twitch.say(channel, "https://bradgarropy.com/store")
     }
 })
